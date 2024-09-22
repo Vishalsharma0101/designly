ff1b4acc82b4becb0769d54808442d7b96d5d528
Vishal Sharma <fvishalsharm123@gmail.com>
2024-09-22 10:17:30 +0530

    Sercer Issue fixed

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ if(process.env.NODE_ENV === 'local'){
 }
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname , "./frontend")));
+    app.use(express.static(path.join(__dirname , "./frontend/dist")));
     app.get("*" , (req , res) => {
-        res.sendFile(path.resolve(__dirname , './','frontend', 'index.html'))
+        res.sendFile(path.resolve(__dirname , './','frontend', 'dist' ,'index.html'))
     })
 }
 
